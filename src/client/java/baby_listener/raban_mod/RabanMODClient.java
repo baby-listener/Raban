eca8b3e832863186e042ddfbf4c30eebd8c0c276
baby-listener <06masanobu21@gmail.com>
2026-01-20 22:36:45 +0900

    テンプレート共有

---
+++ src/client/java/baby_listener/raban_mod/RabanMODClient.java
@@ -0,0 +1,10 @@
+package baby_listener.raban_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RabanMODClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
